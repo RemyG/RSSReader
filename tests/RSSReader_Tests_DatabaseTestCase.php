adae246744ea69fda50adaf84c55063e08c062d9
RemyG <remy.gardette@gmail.com>
2014-09-30 15:41:50 +0100

    New test files

---
+++ tests/RSSReader_Tests_DatabaseTestCase.php
@@ -0,0 +1,58 @@
+<?php
+abstract class RSSReader_Tests_DatabaseTestCase extends PHPUnit_Extensions_Database_TestCase
+{
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
+
+    // only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
+    private $conn = null;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO('sqlite::memory:');
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, ':memory:');
+        }
+
+        return $this->conn;
+    }
+
+    public function setUp()
+    {
+        $conn = $this->getConnection();
+        $pdo = $conn->getConnection();
+
+        // set up tables
+        $fixtureDataSet = $this->getDataSet($this->fixtures);
+        foreach ($fixtureDataSet->getTableNames() as $table) {
+            // drop table
+            $pdo->exec("DROP TABLE IF EXISTS `$table`;");
+            // recreate table
+            $meta = $fixtureDataSet->getTableMetaData($table);
+            $create = "CREATE TABLE IF NOT EXISTS `$table` ";
+            $cols = array();
+            foreach ($meta->getColumns() as $col) {
+                $cols[] = "`$col` VARCHAR(200)";
+            }
+            $create .= '('.implode(',', $cols).');';
+            $pdo->exec($create);
+        }
+
+        parent::setUp();
+    }
+
+    public function tearDown() {
+        $allTables =
+        $this->getDataSet($this->fixtures)->getTableNames();
+        foreach ($allTables as $table) {
+            // drop table
+            $conn = $this->getConnection();
+            $pdo = $conn->getConnection();
+            $pdo->exec("DROP TABLE IF EXISTS `$table`;");
+        }
+
+        parent::tearDown();
+    }
+}
--- tests/src/controllers/CategoryControllerTest.php
+++ tests/src/controllers/CategoryControllerTest.php
@@ -3,8 +3,20 @@
 /**
  * @covers CategoryController
  */
-class CategoryControllerTest extends PHPUnit_Framework_TestCase
+class CategoryControllerTest extends RSSReader_Tests_DatabaseTestCase
 {
+	public $fixtures = array(
+        'category'
+    );
+
+	/**
+	 * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+	 */
+	public function getDataSet()
+	{
+		return $this->createFlatXmlDataSet(dirname(__FILE__).'/../../resources/category_controller_test.xml');
+	}
+
 	public function testOrder_wrongArguments()
 	{
 		$controller = new CategoryController();
@@ -18,43 +30,15 @@ class CategoryControllerTest extends PHPUnit_Framework_TestCase
 
 	public function testOrder_forward()
 	{
-		$cat = $this->mockCategory(6, 'Test cat 6', 3, 1);
+		$conn = $this->getConnection()->getConnection();
 
-		$cats = array();
-		$cats[] = $this->mockCategory(1, 'Test cat 1', 1, 1); // $i = 0 -> order = 0
-		$cats[] = $this->mockCategory(4, 'Test cat 4', 2, 1); // $i = 1 -> order = 1
-		$cats[] = $this->mockCategory(6, 'Test cat 6', 3, 1); // $i = 2 -> order = 4
-		$cats[] = $this->mockCategory(5, 'Test cat 5', 4, 1); // $i = 2 -> order = 2
-		$cats[] = $this->mockCategory(3, 'Test cat 3', 5, 1); // $i = 3 -> order = 3
-		$cats[] = $this->mockCategory(2, 'Test cat 2', 6, 1); // $i = 4 -> order = 5
-
-		$dao = $this->getMock('CategoryDAO', array('findById', 'findByParentId', 'save'));
-		$dao->expects($this->any())
-				->method('findById')
-				->withAnyParameters()
-				->will($this->returnValue($cat));
-		$dao->expects($this->any())
-				->method('findByParentId')
-				->withAnyParameters()
-				->will($this->returnValue($cats));
-		$dao->expects($this->any())
-				->method('save');
+		$dao = CategoryTestDTO($conn);
 
 		$controller = new CategoryController($dao);
 
 		$controller->logicOrder(6, 4);
 
 		$this->resetModified($cats);
-
-		$result = array();
-		$result[] = $this->mockCategory(1, 'Test cat 1', 0, 1);
-		$result[] = $this->mockCategory(4, 'Test cat 4', 1, 1);
-		$result[] = $this->mockCategory(6, 'Test cat 6', 4, 1);
-		$result[] = $this->mockCategory(5, 'Test cat 5', 2, 1);
-		$result[] = $this->mockCategory(3, 'Test cat 3', 3, 1);
-		$result[] = $this->mockCategory(2, 'Test cat 2', 5, 1);
-
-		$this->assertEquals($result, $cats);
 	}
 
 	public function testOrder_backward()
@@ -148,6 +132,25 @@ class CategoryControllerTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals('Error while saving', $output['error']);
 	}
 
+	public function testLoad()
+	{
+		$cat = $this->mockCategory(1, 'Test cat 1', 1, 1);
+
+		$entries = array();
+		$entries[] = $this->mockEntry(1, "Entry 1");
+		$entries[] = $this->mockEntry(2, "Entry 2");
+		$entries[] = $this->mockEntry(3, "Entry 3");
+		$entries[] = $this->mockEntry(4, "Entry 4");
+
+		$dao = $this->getMock('CategoryDAO', array('findById', 'getUnreadEntries'));
+		$dao->expects($this->any())
+				->method('findById')
+				->will($this->returnValue($cat));
+		$dao->expects($this->any())
+				->method('findById')
+				->will($this->returnValue($cat));
+	}
+
 	private function mockCategory($id, $name, $catOrder, $parentCat)
 	{
 		$cat = new Category();
@@ -159,12 +162,22 @@ class CategoryControllerTest extends PHPUnit_Framework_TestCase
 		return $cat;
 	}
 
+	private function mockEntry($id, $title)
+	{
+		$entry = new Entry();
+		$entry->setId($id);
+		$entry->setTitle($title);
+		return $entry;
+	}
+
 	private function resetModified($objects)
 	{
 		foreach ($objects as $object) {
 			$object->resetModified();
 		}
 	}
+
+	
 }
 
 ?>
